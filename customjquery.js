d8008ebfdb0b25cfedaa41ae5287b893ecfb8754
Ersin Dagli <41599748+ErsinDagli@users.noreply.github.com>
2018-08-05 20:15:33 +1000

    glyphicon update

--- customjquery.js
+++ customjquery.js
@@ -13,15 +13,27 @@ $(document).ready(() => {
 
 // }   
 //  });
-  var bodyPage = $('body');
+
+//prevent image right click saveAs
+
+$('img').mousedown(function (e) {
+  if(e.button == 2) { // right click
+    return false; // do nothing!
+  }
+})
+
+var bodyPage = $('body');
 $('#navButton').on('click', () => {
   if (bodyPage.hasClass('left')){
+    //change hamburger menu to cross remove icon
+  $("#hamburgermenu").removeClass("fas fa-times").addClass("fas fa-bars");
   bodyPage.animate({'margin-right':'0px'});
   $('nav').animate({'margin-right': '0px'});
   $('#sidemenu').animate({'right':'-350px'});
   bodyPage.removeClass('left');
   }
   else {
+    $("#hamburgermenu").removeClass("fas fa-bars").addClass("fas fa-times");
     bodyPage.animate({'margin-right':'350px'});
     $('nav').animate({'margin-right': '350px'});
     bodyPage.addClass('left');
@@ -29,44 +41,50 @@ $('#navButton').on('click', () => {
   }
 
 });
+var bodyPage = $('body');
 
- var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 210) {
-                  $(".menuicon").attr('src', 'images/menu.jpg'); 
-                    $("nav").css('background-color', 'white');
-                    $("nav a").css('color', 'black');
+
+
+  var scroll_pos = 0;
+  $(document).scroll(function() { 
+                  scroll_pos = $(this).scrollTop();
+                  if(scroll_pos > 210) {
+                   
+                      $("nav").css('background-color', 'white');
+                      $("nav a").css('color', 'black');
+                      
+                      //swap from black to grey
+                      $('nav a').hover(function () {
+                        $(this).css('color', 'grey');
+                      },
+                      function () {
+                        $(this).css('color', 'black');
+                      });
+            //swapping menu icon ED from white to orange as you scroll down
+                      $('#icon').attr('src', 'images/orangelabel.png');
+  
+                     
+                  } else if (scroll_pos <= 210 ){
+                   
+                    $("nav").css('background-color', 'black');
+                    $("nav a").css('color', 'grey');
                     
-                    //swap from black to grey
+                    //swap from grey to white
                     $('nav a').hover(function () {
-                      $(this).css('color', 'grey');
+                      $(this).css('color', 'white');
                     },
                     function () {
-                      $(this).css('color', 'black');
+                      $(this).css('color', 'grey');
                     });
-          //swapping menu icon ED from white to orange as you scroll down
-                    $('#icon').attr('src', 'images/orangelabel.png');
+                    //swapping menu icon ED from orange to white as you scroll down
+                    $('#icon').attr('src', 'images/whitelabel.png');
+                  } 
+              
+          
+  });
 
-                   
-                } else if (scroll_pos <= 210 ){
-                 $(".menuicon").attr('src', 'images/menuwhite.jpg');
-                  $("nav").css('background-color', 'black');
-                  $("nav a").css('color', 'grey');
-                  
-                  //swap from grey to white
-                  $('nav a').hover(function () {
-                    $(this).css('color', 'white');
-                  },
-                  function () {
-                    $(this).css('color', 'grey');
-                  });
-                  //swapping menu icon ED from orange to white as you scroll down
-                  $('#icon').attr('src', 'images/whitelabel.png');
-                } 
-            
-        
-});
+
+
 
 
 //hover over text in navigation bar change scolour from grey to white
@@ -97,15 +115,19 @@ function hideNavBars(){if($(window).width() <= 992){
 
   
   navbar.addClass('invisible')
-  sidenavbar.removeClass('invisible')
+  sidenavbar.removeClass('invisible');
   sidenavbar.css('height', 100);
+  $('#sidenavbarseperator').removeClass('invisible');
+ 
 
 } else if ($(window).width() >= 992){
 
     
     navbar.removeClass('invisible');
-    sidenavbar.addClass('invisible')
+    sidenavbar.addClass('invisible');
     sidenavbar.css('height', 0);
+    $('#sidenavbarseperator').addClass('invisible');
+   
 
  }}
 
@@ -114,10 +136,10 @@ hideNavBars();
 $(window).resize(() => {hideNavBars()});
   
 
+
+
+
 });
-
-
-
 
 
 
